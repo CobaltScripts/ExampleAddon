2355f24667e599559240b6827b4aee9e08effd6c
Nathan <209938737+quiteboring@users.noreply.github.com>
2025-11-04 09:42:11 -0500

    feat: new category system

--- src/main/kotlin/com/example/ExampleAddon.kt
+++ src/main/kotlin/com/example/ExampleAddon.kt
@@ -4,11 +4,12 @@ import com.example.command.ExampleCommand
 import com.example.module.ExampleModule
 import org.cobalt.api.addon.Addon
 import org.cobalt.api.command.CommandManager
+import org.cobalt.api.module.Category
 import org.cobalt.api.module.ModuleManager
 
 object ExampleAddon : Addon() {
 
-  const val CATEGORY = "Example"
+  val CATEGORY = Category("Example", "/assets/exampleaddon/icon.svg")
 
   override fun onInitialize() {
     ModuleManager.addModules(ExampleModule)
