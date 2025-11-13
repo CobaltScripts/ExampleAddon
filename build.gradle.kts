b515388babf013d76a6ded7c48f50fc132459a01
oblongboot <81798709+oblongboot@users.noreply.github.com>
2025-11-13 22:42:43 +0000

    feat: change example mod to support hot reloading

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ repositories {
   maven("https://maven.meteordev.org/releases")
   maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
   maven("https://api.modrinth.com/maven")
+  maven("https://mvn.oblongboot.dev/")
 }
 
 dependencies {
@@ -33,8 +34,7 @@ dependencies {
 
   modImplementation("net.fabricmc:fabric-language-kotlin:${property("fabric_kotlin_version")}")
   modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
-  modImplementation(files("lib/Cobalt-1.0.0.jar"))
-
+  modImplementation("org.cobalt:cobalt:0.0.1")
   modRuntimeOnly("me.djtheredstoner:DevAuth-fabric:1.2.1")
   implementation("meteordevelopment:discord-ipc:1.1")
   modImplementation("org.lwjgl:lwjgl-nanovg:${lwjglVersion}")
