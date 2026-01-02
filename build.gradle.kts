81214232dfa31bd52a08c6baa397fca8e509be39
Nathan <209938737+quiteboring@users.noreply.github.com>
2026-01-02 14:28:51 -0500

    chore: update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,11 +36,12 @@ dependencies {
   modImplementation("org.cobalt:cobalt:1.0.0")
   modImplementation("meteordevelopment:discord-ipc:1.1")
   modImplementation("org.reflections:reflections:0.10.2")
-  modImplementation("org.lwjgl:lwjgl-nanovg:${lwjglVersion}")
 
-  listOf("windows", "linux", "macos", "macos-arm64").forEach {
-    modImplementation("org.lwjgl:lwjgl-nanovg:${lwjglVersion}:natives-$it")
-  }
+  modImplementation("org.lwjgl:lwjgl-nanovg:${lwjglVersion}")
+  modImplementation("org.lwjgl:lwjgl-nanovg:${lwjglVersion}:natives-windows")
+  modImplementation("org.lwjgl:lwjgl-nanovg:${lwjglVersion}:natives-linux")
+  modImplementation("org.lwjgl:lwjgl-nanovg:${lwjglVersion}:natives-macos")
+  modImplementation("org.lwjgl:lwjgl-nanovg:${lwjglVersion}:natives-macos-arm64")
 
   modRuntimeOnly("me.djtheredstoner:DevAuth-fabric:1.2.1")
 }
